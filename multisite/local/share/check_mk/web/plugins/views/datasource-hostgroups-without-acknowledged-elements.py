0c0f3caf85052c8388828a681d8854bda0959e80
Tobias <faxmail09@gmx.net>
2016-10-09 01:09:47 +0200

    compacted code

--- multisite/local/share/check_mk/web/plugins/views/datasource-hostgroups-without-acknowledged-elements.py
+++ multisite/local/share/check_mk/web/plugins/views/datasource-hostgroups-without-acknowledged-elements.py
@@ -1,41 +1,36 @@
 
 # a quick hack(tm)
 
-def dontspoilthenamespace_get_ack_hg_data(columns, query, only_sites, limit, all_active_filters):
+from collections import defaultdict
+
+def hgnoack_nested_dict_factory():
+  return defaultdict(int)
+def hgnoack_nested_dict_factory2():
+  return defaultdict(hgnoack_nested_dict_factory)
+
+def hgnoack_get_data(columns, query, only_sites, limit, all_active_filters):
 	datasource = multisite_datasources["hostgroups-without-acknowledged-elements"].copy()
 	datasource["table"] = "hostgroups"
 	hostgroups   = query_data(datasource, columns, [], query, only_sites, limit)
 	service_data = query_data(
-				multisite_datasources["services-acknowledged-elements"],
+				multisite_datasources["services-without-acknowledged-elements"],
 				["display_name", "host_name","host_groups","display_name","state","state_type","acknowledged"],
 				[],"",only_sites,limit)
-	host_data = query_data( multisite_datasources["hosts-acknowledged-elements"],
+	host_data = query_data( multisite_datasources["hosts-without-acknowledged-elements"],
 				["host_name", "host_groups","state","state_type","acknowledged"],
 				[],"",only_sites,limit)
 
 	svc_const  = { 1 : "warn", 2 : "crit"    }
 	host_const = { 1 : "down", 2 : "unreach" } 
-	ack={}
+	ack=defaultdict(hgnoack_nested_dict_factory2)
+
 	for svc in service_data:	
 		for hg in svc["host_groups"]:
-			if not hg in ack: 
-				ack[hg]={}
-			if not "services" in ack[hg]: 
-				ack[hg]["services"]={}
-			if not svc["state"] in ack[hg]["services"]: 
-				ack[hg]["services"][svc["state"]]=1
-			else:
-				ack[hg]["services"][svc["state"]]=ack[hg]["services"][svc["state"]]+1
+			ack[hg]["services"][svc["state"]]+=1
+			
 	for host in host_data:	
 		for hg in host["host_groups"]:
-			if not hg in ack: 
-				ack[hg]={}
-			if not "hosts" in ack[hg]: 
-				ack[hg]["hosts"]={}
-			if not host["state"] in ack[hg]["hosts"]: 
-				ack[hg]["hosts"][host["state"]]=1
-			else:
-				ack[hg]["hosts"][host["state"]]=ack[hg]["hosts"][host["state"]]+1
+			ack[hg]["hosts"][svc["state"]]+=1
 
 	ind=0
 	for hg in hostgroups:
@@ -55,7 +50,7 @@ def dontspoilthenamespace_get_ack_hg_data(columns, query, only_sites, limit, all
 
 	return hostgroups
 
-multisite_datasources["services-acknowledged-elements"] = {
+multisite_datasources["services-without-acknowledged-elements"] = {
     "title"   : _("Services"),
     "table"   : "services",
     "add_headers" : "Filter: state =~ 0\nFilter: acknowledged = 1\n",
@@ -64,7 +59,7 @@ multisite_datasources["services-acknowledged-elements"] = {
     "idkeys"  : [ "site", "display_name" ],
 }
 
-multisite_datasources["hosts-acknowledged-elements"] = {
+multisite_datasources["hosts-without-acknowledged-elements"] = {
     "title"   : _("Hosts"),
     "table"   : "hosts",
     "add_headers" : "Filter: state =~ 0\nFilter: acknowledged = 1\n",
@@ -75,7 +70,7 @@ multisite_datasources["hosts-acknowledged-elements"] = {
 
 multisite_datasources["hostgroups-without-acknowledged-elements"] = {
     "title"   : _("Hostgroups"),
-    "table"   : lambda columns,query,only_sites,limit,all_active_filters: dontspoilthenamespace_get_ack_hg_data(columns,query,only_sites,limit,all_active_filters),
+    "table"   : lambda columns,query,only_sites,limit,all_active_filters: hgnoack_get_data(columns,query,only_sites,limit,all_active_filters),
     "infos"   : [ "hostgroup" ],
     "keys"    : [ "hostgroup_name" ],
     "idkeys"  : [ "site", "hostgroup_name" ],
